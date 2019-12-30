35dd2b2a8167ca611ff658dd599692f0b1c73b97
Victor Lindh <Victor.Lindh@yh.nackademin.se>
2019-12-30 17:20:25 +0100

    final v.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputNumber = document.getElementById("inputNumber");
 const inputRange = document.getElementById("inputRange");
-
+let finalPassword = document.getElementById("passw");
 inputNumber.addEventListener("input", syncNumber);
 inputRange.addEventListener("input", syncNumber);
 
@@ -9,30 +9,44 @@ function syncNumber(e){
     let uniValue = e.target.value;
     inputNumber.value = uniValue;
     inputRange.value = uniValue;
+    return uniValue;
 }
 
 const uppercase_char_codes = arrayFromLowToHigh(65, 90);
 const lowercase_char_codes = arrayFromLowToHigh(97, 122);
-const symbols_char_codes = arrayFromLowToHigh(33,47);
 const numbers_char_codes = arrayFromLowToHigh(48, 57);
-
-console.log(String.fromCharCode(numbers_char_codes));
-console.log(String.fromCharCode(symbols_char_codes));
+const symbols_char_codes = arrayFromLowToHigh(33,47)
+                            .concat(arrayFromLowToHigh(58,64)
+                            .concat(arrayFromLowToHigh(91,96)
+                            .concat(arrayFromLowToHigh(123,126))))
 
 const generatePasswordBtn = document.getElementById("generatePasswordBtn");
 const form = document.getElementById("form");
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    let includeLower = document.getElementById("includeLower").checked;
-    let includeUpper = document.getElementById("includeUpper").checked;
-    let includeSymbols = document.getElementById("includeSymbols").checked;
-    let includeNumbers = document.getElementById("includeNumbers").checked;
-    const password = generatePassword(includeLower, includeUpper, includeSymbols, includeNumbers)
+    const characterAmount = inputNumber.value;
+    const includeUpper = document.getElementById("includeUpper").checked;
+    const includeSymbols = document.getElementById("includeSymbols").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const password = generatePassword(characterAmount, includeUpper, includeSymbols, includeNumbers)
 })
 
-function generatePassword(includeLower, includeUpper, includeSymbols, includeNumbers){
-        console.log(uppercase_char_codes);
+function generatePassword(characterAmount, includeUpper, includeSymbols, includeNumbers){
+    characterAmount = inputNumber.value;
+    let defaultCharCodes = lowercase_char_codes;
+    if(includeUpper) defaultCharCodes = defaultCharCodes.concat(uppercase_char_codes)
+    if(includeNumbers) defaultCharCodes = defaultCharCodes.concat(numbers_char_codes)
+    if(includeSymbols) defaultCharCodes = defaultCharCodes.concat(symbols_char_codes)
+
+    const passwordCharacters = [];
+    for (let i = 0; i < characterAmount; i++) {
+        const character = defaultCharCodes[Math.floor(Math.random() * defaultCharCodes.length)]
+        passwordCharacters.push(String.fromCharCode(character))
+    }
+    let final = passwordCharacters.join('');
+    finalPassword.innerText = final;
+
 }
 
 function arrayFromLowToHigh(low, high){
